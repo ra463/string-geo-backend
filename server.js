569fe256762961e572f11436bffb9e19e4e25128
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2023-12-19 14:22:22 +0530

    add clustering

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const app = require("./app");
 const { connectDB } = require("./config/database");
-
+const cluster = require("cluster");
+const totalCpus = require("os").cpus();
 connectDB();
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+if (cluster.isMaster) {
+  totalCpus.forEach(async (node)=>{
+    await cluster.fork();
+  })
+  cluster.on("exit",async (worker,signal)=>{
+    console.log("worker is died with pid ",worker.process.pid);
+    await cluster.fork();
+  })
+} else {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
