c9180134c0f49656cdca87b063cc6481a3d47633
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2023-12-19 15:47:19 +0530

    add rate limit

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
 const app = express();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
 dotenv.config({
   path: "./config/config.env",
 });
 
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("tiny"));
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 50,
+  message: "Too many requests from this IP, please try again later.",
+  headers: false,
+});
+app.use(limiter);
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
