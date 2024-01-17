2a419ea4e7b66f1e51166ba9e7b12109fb0fbd0a
Rachit Patel <rachitp509@gmail.com>
2024-01-17 11:22:00 +0530

    bug fixes

--- @transaction_entity/transaction.routes.js
+++ @transaction_entity/transaction.routes.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router();
 
-router.route("/get-all-transaction").get(auth, isAdmin, getAllTransaction);
+router.route("/get-all-transaction").get(getAllTransaction);
 router
   .route("/get-transaction/:transactionId")
   .get(auth, isAdmin, getTransactionById);
