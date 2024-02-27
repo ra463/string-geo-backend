f765e9d42be5cd6d922627fd387115aaf90ba8a5
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-02-27 10:58:19 +0530

    bug fixing

--- @genre_entity/genre.routes.js
+++ @genre_entity/genre.routes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { isAdmin, auth } = require("../middlewares/auth");
-const { createGenre, getGenres, deleteGenre, getGenre, updateGenre } = require("./genre.controller");
+const {
+  createGenre,
+  getGenres,
+  deleteGenre,
+  getGenre,
+  updateGenre,
+} = require("./genre.controller");
 
 const router = express.Router();
 
 router.post("/create-genre", auth, isAdmin, createGenre);
-router.get("/get-genres", auth,  getGenres);
+router.get("/get-genres", getGenres);
 router.delete("/delete-genre/:id", auth, isAdmin, deleteGenre);
 router.get("/get-genre/:id", auth, isAdmin, getGenre);
 router.patch("/update-genre/:id", auth, isAdmin, updateGenre);
