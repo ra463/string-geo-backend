509b4d630befb36ba3911f71efc6db9de78ca302
Rachit Patel <rachitp509@gmail.com>
2024-02-27 14:34:38 +0530

    bug fixes

--- @carousel_entity/carousel.controller.js
+++ @carousel_entity/carousel.controller.js
@@ -51,7 +51,7 @@ exports.deleteCarousel = catchAsyncError(async (req, res, next) => {
   if (!carousel) return next(new ErrorHandler("Carousel not found", 404));
 
   const url = carousel.poster_url;
-  const [] = Promise.all([s3delete(url), carousel.deleteOne()]);
+  await Promise.all([s3delete(url), carousel.deleteOne()]);
 
   res.status(200).json({
     success: true,
