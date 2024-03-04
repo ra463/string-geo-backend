afb21640821292e946940979594cc2b974eceff5
Rachit Patel <rachitp509@gmail.com>
2024-03-04 23:10:59 +0530

    changed the error message

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,9 +1,9 @@
 exports.error = (err, req, res, next) => {
-  
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
   if (err.code === 11000) {
-    err.message = "Plan name already exist";
+    const field = Object.keys(err.keyValue)[0];
+    err.message = `${field} already exists`;
   }
 
   res.status(err.statusCode).json({
