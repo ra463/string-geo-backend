3bc54543a49708f1816a4dd25c2af2ecdb297819
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-03-04 13:54:45 +0530

    changes

--- @order_entity/order.controller.js
+++ @order_entity/order.controller.js
@@ -86,6 +86,20 @@ exports.createOrder = catchAsyncError(async (req, res, next) => {
     }
 
     if (
+      active.plan_name === "Individual" &&
+      active.plan_type === "monthly" &&
+      plan.name === "Individual" &&
+      p_type.plan_type === "monthly"
+    ) {
+      return next(
+        new ErrorHandler(
+          "You Already Have an Active Plan",
+          400
+        )
+      );
+    }
+
+    if (
       active.plan_name === "Family" &&
       active.plan_type === "monthly" &&
       plan.name === "Individual" &&
@@ -262,6 +276,20 @@ exports.createPayapalOrder = catchAsyncError(async (req, res, next) => {
       return next(
         new ErrorHandler(
           "You can upgrade your plan from Individual annual to Family annual only",
+          400
+        )
+      );
+    }
+
+    if (
+      active.plan_name === "Individual" &&
+      active.plan_type === "monthly" &&
+      plan.name === "Individual" &&
+      p_type.plan_type === "monthly"
+    ) {
+      return next(
+        new ErrorHandler(
+          "You Already Have an Active Plan",
           400
         )
       );
