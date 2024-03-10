82aa288f7b09316c8c2cf610768e53c976ca18c2
quantumItInnovations <125892289+quantumItInnovations@users.noreply.github.com>
2024-03-10 23:02:53 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -159,149 +159,149 @@ exports.sendBulkEmail = async (emails, subject, description) => {
   });
 };
 
-// exports.sendInvoice2 = async (user, transaction) => {
-//   return new Promise(async (resolve, reject) => {
-//     // transaction.createAt = "2024-02-21T14:37:29.797+00:00";
-//     const date = new Date(transaction.createAt);
+exports.sendInvoice = async (user, transaction) => {
+  return new Promise(async (resolve, reject) => {
+    // transaction.createAt = "2024-02-21T14:37:29.797+00:00";
+    const date = new Date(transaction.createAt);
 
-//     const options = { year: "numeric", month: "short", day: "2-digit" };
-//     const formattedDate = date.toLocaleDateString("en-US", options);
-//     const htmlTemplate = `<div
-//    style="padding:3rem; margin-bottom:3rem; border:1.5px solid black; margin-top: 7%; margin-left: 7%;margin-right: 7%;">
-//    <div style="text-align: right;">
-//      <img width="200px" height="60px" style="background-color: white; border: none;"
-//        src="https://stringgeo.com/upload/NewFolder/String%20Geo%20logo%20Icon.png" />
-//    </div>
-//    <div>
-//      <p style="margin-bottom: 0.4rem;">
-//        <strong>STRING ART PRIVATE LIMITED</strong>
-//      </p>
-//      <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">
-//        <strong>GSTIN - </strong>37ABICS6540H1Z2
-//      </p>
-//      <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">
-//        <strong>Mobile - </strong>7022022728
-//      </p>
-//      <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">
-//        <strong>Email - </strong>namaskaram@stringgeo.com
-//      </p>
-//    </div>
-//    <div style="text-align: center;">
-//      <h1>Tax Invoice</h1>
-//    </div>
-//    <div style="display: flex;justify-content: space-between;">
-//      <div style="display: flex;flex-direction: column;">
-//        <p style="margin-bottom: 0.2rem;">Billing To: Prakash</p>
-//        <p style="margin-bottom: 0.2rem;margin-top: 0.2rem;">Full Name: ${user.name}</p>
-//        <p style="margin-bottom: 0.2rem;margin-top: 0.2rem;">Email Id: ${user.email}</p>
-//        <p style="margin-top: 0.2rem;">Contact No: ${user.mobile}</p>
-//      </div>
-//      <div style="display: flex;flex-direction: column;">
-//        <p style="margin-top: 0.2rem;margin-bottom: 0.2rem;" style="text-align: end;">Transaction Date: ${formattedDate}</p>
-//        <p style="margin-top: 0.2rem;">Transaction No: ${transaction.payment_id}</p>
-//      </div>
-//    </div>
-//    <div>
-//      <table style="margin-top: 1rem; border-collapse: collapse; width: 100%; border: 3px solid black;">
-//        <thead>
-//          <tr>
-//            <th style="border: 2px solid black; padding: 8px;" colspan="6">Description</th>
-//            <th style="border: 2px solid black; padding: 8px;" colspan="3">SAC Code</th>
-//            <th style="border: 2px solid black; padding: 8px;" colspan="3">Amount (Rs.)</th>
-//          </tr>
-//        </thead>
-//        <tbody>
-//          <tr>
-//            <td style="border: 2px solid black; padding: 8px; text-align: center;" colspan="6">Basic
-//              (Monthly)</td>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">998433</td>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">${parseFloat(
-//               0.82 * transaction.amount
-//             ).toFixed(2)}</td>
-//          </tr>
-//          <tr>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="6">IGST @ 18%
-//            </td>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3"></td>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">${parseFloat(
-//               0.18 * transaction.amount
-//             ).toFixed(2)}</td>
-//          </tr>
-//          <tr>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="6">Invoice Total
-//            </td>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3"></td>
-//            <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">${
-//               transaction.amount
-//             }</td>
-//          </tr>
+    const options = { year: "numeric", month: "short", day: "2-digit" };
+    const formattedDate = date.toLocaleDateString("en-US", options);
+    const htmlTemplate = `<div
+   style="padding:3rem; margin-bottom:3rem; border:1.5px solid black; margin-top: 7%; margin-left: 7%;margin-right: 7%;">
+   <div style="text-align: right;">
+     <img width="200px" height="60px" style="background-color: white; border: none;"
+       src="https://stringgeo.com/upload/NewFolder/String%20Geo%20logo%20Icon.png" />
+   </div>
+   <div>
+     <p style="margin-bottom: 0.4rem;">
+       <strong>STRING ART PRIVATE LIMITED</strong>
+     </p>
+     <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">
+       <strong>GSTIN - </strong>37ABICS6540H1Z2
+     </p>
+     <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">
+       <strong>Mobile - </strong>7022022728
+     </p>
+     <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">
+       <strong>Email - </strong>namaskaram@stringgeo.com
+     </p>
+   </div>
+   <div style="text-align: center;">
+     <h1>Tax Invoice</h1>
+   </div>
+   <div style="display: flex;justify-content: space-between;">
+     <div style="display: flex;flex-direction: column;">
+       <p style="margin-bottom: 0.2rem;">Billing To: Prakash</p>
+       <p style="margin-bottom: 0.2rem;margin-top: 0.2rem;">Full Name: ${user.name}</p>
+       <p style="margin-bottom: 0.2rem;margin-top: 0.2rem;">Email Id: ${user.email}</p>
+       <p style="margin-top: 0.2rem;">Contact No: ${user.mobile}</p>
+     </div>
+     <div style="display: flex;flex-direction: column;">
+       <p style="margin-top: 0.2rem;margin-bottom: 0.2rem;" style="text-align: end;">Transaction Date: ${formattedDate}</p>
+       <p style="margin-top: 0.2rem;">Transaction No: ${transaction.payment_id}</p>
+     </div>
+   </div>
+   <div>
+     <table style="margin-top: 1rem; border-collapse: collapse; width: 100%; border: 3px solid black;">
+       <thead>
+         <tr>
+           <th style="border: 2px solid black; padding: 8px;" colspan="6">Description</th>
+           <th style="border: 2px solid black; padding: 8px;" colspan="3">SAC Code</th>
+           <th style="border: 2px solid black; padding: 8px;" colspan="3">Amount (Rs.)</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td style="border: 2px solid black; padding: 8px; text-align: center;" colspan="6">Basic
+             (Monthly)</td>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">998433</td>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">${parseFloat(
+              0.82 * transaction.amount
+            ).toFixed(2)}</td>
+         </tr>
+         <tr>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="6">IGST @ 18%
+           </td>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3"></td>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">${parseFloat(
+              0.18 * transaction.amount
+            ).toFixed(2)}</td>
+         </tr>
+         <tr>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="6">Invoice Total
+           </td>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3"></td>
+           <td style="border: 2px solid black; padding: 8px;text-align: center;" colspan="3">${
+              transaction.amount
+            }</td>
+         </tr>
 
-//        </tbody>
-//      </table>
-//    </div>
+       </tbody>
+     </table>
+   </div>
 
-//    <div>
-//      <p style="margin-bottom: 0.4rem;"><b>Note:</b> The subscription amount is inclusive Goods and Service tax
-//        (GST) at rate of 18%.</p>
-//      <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">Reverse Charge Applicability: No</p>
-//      <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">See Terms and Conditions on the www.stringgeo.com
-//        website</p>
-//    </div>
+   <div>
+     <p style="margin-bottom: 0.4rem;"><b>Note:</b> The subscription amount is inclusive Goods and Service tax
+       (GST) at rate of 18%.</p>
+     <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">Reverse Charge Applicability: No</p>
+     <p style="margin-top: 0.4rem;margin-bottom: 0.4rem;">See Terms and Conditions on the www.stringgeo.com
+       website</p>
+   </div>
 
-//    <div style="margin-top: 4rem;text-align: center;">
-//      <p>This is System generated invoice</p>
-//      <p style="margin-bottom: 0.3rem;"><b>STRING ART PRIVATE LIMITED</b></p>
-//      <p style="margin-bottom: 0.3rem;margin-top: 0.3rem;">D NO 85-40-4/4, F S-1, SRI SARASWATHI NIVAS APPT,</p>
-//      <p style="margin-bottom: 0.3rem;margin-top: 0.3rem;">RAJAHMUNDRY, East Godavari,</p>
-//      <p style="margin-bottom: 0.3rem;margin-top: 0.3rem;">Andhra Pradesh, India, 533101</p>
+   <div style="margin-top: 4rem;text-align: center;">
+     <p>This is System generated invoice</p>
+     <p style="margin-bottom: 0.3rem;"><b>STRING ART PRIVATE LIMITED</b></p>
+     <p style="margin-bottom: 0.3rem;margin-top: 0.3rem;">D NO 85-40-4/4, F S-1, SRI SARASWATHI NIVAS APPT,</p>
+     <p style="margin-bottom: 0.3rem;margin-top: 0.3rem;">RAJAHMUNDRY, East Godavari,</p>
+     <p style="margin-bottom: 0.3rem;margin-top: 0.3rem;">Andhra Pradesh, India, 533101</p>
 
-//    </div>
+   </div>
 
-//  </div>`;
+ </div>`;
 
-//     try {
-//       const browser = await puppeteer.launch({
-//         // headless:false,
-//         userDataDir: join(__dirname, ".cache", `puppeteer`,`${user._id}`),
-//         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-//       });
-//       const page = await browser.newPage();
+    try {
+      const browser = await puppeteer.launch({
+        // headless:false,
+        userDataDir: join(__dirname, ".cache", `puppeteer`,`${user._id}`),
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+      const page = await browser.newPage();
 
-//       // Set content to the page
-//       await page.setContent(htmlTemplate);
+      // Set content to the page
+      await page.setContent(htmlTemplate);
 
-//       // Generate PDF from the page content
-//       const pdfBuffer = await page.pdf({ format: "A4" });
+      // Generate PDF from the page content
+      const pdfBuffer = await page.pdf({ format: "A4" });
 
-//       // Close the browser
-//       await browser.close();
+      // Close the browser
+      await browser.close();
 
-//       const msg = {
-//         to: user.email,
-//         from: "namaskaram@stringgeo.com",
-//         subject: "Sending an Invoice",
-//         text: `Welcome ${user.name}, Thank you for joining us. Please find an attachment below containing your transaction details.`,
-//         attachments: [
-//           {
-//             content: pdfBuffer.toString("base64"),
-//             filename: `${user._id}.pdf`,
-//             encoding: "base64",
-//           },
-//         ],
-//       };
+      const msg = {
+        to: user.email,
+        from: "namaskaram@stringgeo.com",
+        subject: "Sending an Invoice",
+        text: `Welcome ${user.name}, Thank you for joining us. Please find an attachment below containing your transaction details.`,
+        attachments: [
+          {
+            content: pdfBuffer.toString("base64"),
+            filename: `${user._id}.pdf`,
+            encoding: "base64",
+          },
+        ],
+      };
 
-//       await sg.send(msg);
-//       fs.rmdirSync(join(__dirname, ".cache", `puppeteer`,`${user._id}`), {
-//         recursive: true,
-//       });
+      await sg.send(msg);
+      fs.rmdirSync(join(__dirname, ".cache", `puppeteer`,`${user._id}`), {
+        recursive: true,
+      });
 
-//       resolve(pdfBuffer);
-//     } catch (error) {
-//       console.error("Error generating PDF:", error);
-//       reject(error);
-//     }
-//   });
-// };
+      resolve(pdfBuffer);
+    } catch (error) {
+      console.error("Error generating PDF:", error);
+      reject(error);
+    }
+  });
+};
 
 // exports.sendInvoice = async (user, transaction) => {
 //   return new Promise(async (resolve, reject) => {
@@ -457,93 +457,93 @@ exports.sendBulkEmail = async (emails, subject, description) => {
 //   });
 // };
 
-exports.sendInvoice = async (user, transaction) => {
-  return new Promise(async (resolve, reject) => {
-    const htmlTemplate = fs.readFileSync(
-      path.join(__dirname, "invoice.html"),
-      "utf8"
-    );
-    const formatDateTime = (dateTimeString) => {
-      const dateTime = new Date(dateTimeString);
-      const month = dateTime.toLocaleString("default", { month: "short" });
-      const day = dateTime.getDate();
-      const year = dateTime.getFullYear();
-      const time = dateTime.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-      });
-      return `${day} ${month}, ${year}`;
-    };
+// exports.sendInvoice = async (user, transaction) => {
+//   return new Promise(async (resolve, reject) => {
+//     const htmlTemplate = fs.readFileSync(
+//       path.join(__dirname, "invoice.html"),
+//       "utf8"
+//     );
+//     const formatDateTime = (dateTimeString) => {
+//       const dateTime = new Date(dateTimeString);
+//       const month = dateTime.toLocaleString("default", { month: "short" });
+//       const day = dateTime.getDate();
+//       const year = dateTime.getFullYear();
+//       const time = dateTime.toLocaleTimeString("en-US", {
+//         hour: "numeric",
+//         minute: "numeric",
+//         hour12: true,
+//       });
+//       return `${day} ${month}, ${year}`;
+//     };
 
-    // Replace placeholders with actual data in HTML template
-    const formattedHtml = htmlTemplate
-      .replace("${user.name}", user.name)
-      .replace("${user.email}", user.email)
-      .replace("${user.mobile}", user.mobile)
-      .replace(
-        "${transaction.createdAt}",
-        formatDateTime(transaction.createdAt)
-      )
-      .replace("${transaction.payment_id}", transaction.payment_id)
-      .replace("${transaction.amount}", transaction.amount)
-      .replace("gstamount", parseFloat(0.18 * transaction.amount).toFixed(2))
-      .replace("totalamount", parseFloat(0.82 * transaction.amount).toFixed(2));
+//     // Replace placeholders with actual data in HTML template
+//     const formattedHtml = htmlTemplate
+//       .replace("${user.name}", user.name)
+//       .replace("${user.email}", user.email)
+//       .replace("${user.mobile}", user.mobile)
+//       .replace(
+//         "${transaction.createdAt}",
+//         formatDateTime(transaction.createdAt)
+//       )
+//       .replace("${transaction.payment_id}", transaction.payment_id)
+//       .replace("${transaction.amount}", transaction.amount)
+//       .replace("gstamount", parseFloat(0.18 * transaction.amount).toFixed(2))
+//       .replace("totalamount", parseFloat(0.82 * transaction.amount).toFixed(2));
 
-    // Create PDF options
-    const options = {
-      format: "A4",
-    };
+//     // Create PDF options
+//     const options = {
+//       format: "A4",
+//     };
 
-    // Create PDF file
-    const file = {
-      content: formattedHtml,
-    };
-    const pdfBuffer = await pdf.create(
-      {
-        html: formattedHtml,
-        data: {},
-        path: "./output.pdf",
-        type: "buffer",
-      },
-      options
-    );
+//     // Create PDF file
+//     const file = {
+//       content: formattedHtml,
+//     };
+//     const pdfBuffer = await pdf.create(
+//       {
+//         html: formattedHtml,
+//         data: {},
+//         path: "./output.pdf",
+//         type: "buffer",
+//       },
+//       options
+//     );
 
-    const msg = {
-      to: user.email,
-      from: "namaskaram@stringgeo.com",
-      subject: "Sending an Invoice",
-      html: `<div style="font-family: 'Arial', sans-serif; text-align: center; background-color: #f4f4f4; margin-top: 15px; padding: 0;">
+//     const msg = {
+//       to: user.email,
+//       from: "namaskaram@stringgeo.com",
+//       subject: "Sending an Invoice",
+//       html: `<div style="font-family: 'Arial', sans-serif; text-align: center; background-color: #f4f4f4; margin-top: 15px; padding: 0;">
       
-                      <div style="max-width: 600px; margin: 30px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-                        <h1 style="color: #333333;">Hey ${user.name}! You Payment of ${transaction.amount} has been done successfully</h1>
-                        <p style="color: #666666;">You have now access to our paid content.</p>
-                        <p style="color: #666666;">
-                          If you did not request this mail, please ignore this email.
-                        </p>
-                      </div>
+//                       <div style="max-width: 600px; margin: 30px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+//                         <h1 style="color: #333333;">Hey ${user.name}! You Payment of ${transaction.amount} has been done successfully</h1>
+//                         <p style="color: #666666;">You have now access to our paid content.</p>
+//                         <p style="color: #666666;">
+//                           If you did not request this mail, please ignore this email.
+//                         </p>
+//                       </div>
       
-                      <div style="color: #888888;">
-                        <p style="margin-bottom: 10px;">Regards, <span style="color: #caa257;">Team String Geo</span></p>
-                      </div>
+//                       <div style="color: #888888;">
+//                         <p style="margin-bottom: 10px;">Regards, <span style="color: #caa257;">Team String Geo</span></p>
+//                       </div>
       
-                    </div>`,
-      attachments: [
-        {
-          content: pdfBuffer.toString("base64"),
-          filename: `${user.name}.pdf`,
-          // path: `${user._id}.pdf`,
-          // encoding: "base64",
-        },
-      ],
-    };
+//                     </div>`,
+//       attachments: [
+//         {
+//           content: pdfBuffer.toString("base64"),
+//           filename: `${user.name}.pdf`,
+//           // path: `${user._id}.pdf`,
+//           // encoding: "base64",
+//         },
+//       ],
+//     };
 
-    try {
-      await sg.send(msg);
-      resolve(pdfBuffer);
-    } catch (error) {
-      console.log(error);
-      reject(error);
-    }
-  });
-};
+//     try {
+//       await sg.send(msg);
+//       resolve(pdfBuffer);
+//     } catch (error) {
+//       console.log(error);
+//       reject(error);
+//     }
+//   });
+// };
